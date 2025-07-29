8246a4c33dfd9d993a657068f9a3c19b2c7e1870
codeonce-with-akash <akashodwa4@gmail.com>
2025-07-29 08:57:06 +0530

    added new constants

--- flipkart-java-application/src/main/java/MyConstants.java
+++ flipkart-java-application/src/main/java/MyConstants.java
@@ -1,4 +1,8 @@
 public class MyConstants {
 	public static final String SUCCESS_MSG = "SUCCESS";
+<<<<<<< Updated upstream
 	public static final String FAILUER_MSG = "FAILED";
+=======
+	public static final String QUEUE_NAME = "VC_Q1_PC";
+>>>>>>> Stashed changes
 }
